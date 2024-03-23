62a121e61cfa7560c0428595cfa1dc3e6d151182
Davi-Muta <davimassaru@hotmail.com>
2024-03-23 13:31:21 -0300

    init JAVA

---
+++ src/main/java/br/com/fiap/soat1/t32/consumerreport/ConsumerReportApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.soat1.t32.consumerreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/soat1/t32/consumerreport/ConsumerReportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.soat1.t32.consumerreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
