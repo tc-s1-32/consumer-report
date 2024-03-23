63dca824fe02b3915521007e39467b102d42a9ff
Davi-Muta <davimassaru@hotmail.com>
2024-03-23 17:38:05 -0300

    send email

--- src/main/java/br/com/fiap/soat1/t32/consumerreport/models/user/presenters/EspelhoPontoMensal.java
+++ src/main/java/br/com/fiap/soat1/t32/consumerreport/models/user/presenters/EspelhoPontoMensal.java
@@ -2,6 +2,7 @@ package br.com.fiap.soat1.t32.consumerreport.models.user.presenters;
 
 import java.util.List;
 
+import br.com.fiap.soat1.t32.consumerreport.models.user.Ponto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,37 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class EspelhoPontoMensal {
+    
     private List<EspelhoPontoDiario> espelhoPontoDiarios;
     private long totalMinutos;
+
+    public static String buildHtmlContent(EspelhoPontoMensal espelhoPontoMensal) {
+        StringBuilder htmlContent = new StringBuilder();
+
+        // Início do documento HTML
+        htmlContent.append("<html><body>");
+
+        // Adiciona informações sobre o espelhoPontoMensal
+        htmlContent.append("<h1>Relatório Mensal de Ponto</h1>");
+        htmlContent.append("<p>Total de Minutos: ").append(espelhoPontoMensal.getTotalMinutos()).append("</p>");
+
+        // Adiciona informações sobre os espelhoPontoDiarios
+        List<EspelhoPontoDiario> espelhoPontoDiarios = espelhoPontoMensal.getEspelhoPontoDiarios();
+        for (EspelhoPontoDiario espelhoPontoDiario : espelhoPontoDiarios) {
+            htmlContent.append("<h2>Data: ").append(espelhoPontoDiario.getData()).append("</h2>");
+            htmlContent.append("<p>Total de Minutos: ").append(espelhoPontoDiario.getTotalMinutos()).append("</p>");
+
+            // Adiciona informações sobre os pontos
+            List<Ponto> pontos = espelhoPontoDiario.getPontos();
+            for (Ponto ponto : pontos) {
+                htmlContent.append("<p>Data do Ponto: ").append(ponto.getData()).append("</p>");
+                htmlContent.append("<p>Evento Pronto: ").append(ponto.getEventoPronto()).append("</p>");
+            }
+        }
+
+        // Fim do documento HTML
+        htmlContent.append("</body></html>");
+
+        return htmlContent.toString();
+    }
 }
---
+++ src/main/java/br/com/fiap/soat1/t32/consumerreport/services/EnvioEmailRelatorioPonto.java
@@ -0,0 +1,31 @@
+package br.com.fiap.soat1.t32.consumerreport.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.messaging.MessagingException;
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.soat1.t32.consumerreport.models.user.presenters.EspelhoPontoMensal;
+import jakarta.mail.internet.MimeMessage;
+import lombok.NonNull;
+
+@Service
+public class EnvioEmailRelatorioPonto {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+
+    public void sendReport(EspelhoPontoMensal espelhoPontoMensal, @NonNull String destinatario) throws MessagingException, jakarta.mail.MessagingException {
+
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(destinatario);
+        helper.setSubject("Relatório Mensal de Ponto");
+        helper.setText(EspelhoPontoMensal.buildHtmlContent(espelhoPontoMensal), true);
+
+        emailSender.send(message);
+    }
+
+}
