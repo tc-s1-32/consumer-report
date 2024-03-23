885d27d3d34c7d0ddd5abf83da44e5e2655b1d51
Davi-Muta <davimassaru@hotmail.com>
2024-03-23 13:58:42 -0300

    create Rabbit Template Configuration

--- src/main/java/br/com/fiap/soat1/t32/consumerreport/ConsumerReportApplication.java
+++ src/main/java/br/com/fiap/soat1/t32/consumerreport/ConsumerReportApplication.java
@@ -1,9 +1,13 @@
 package br.com.fiap.soat1.t32.consumerreport;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.fiap.soat1.t32")
+@EnableRabbit
+@EnableFeignClients
 public class ConsumerReportApplication {
 
 	public static void main(String[] args) {
